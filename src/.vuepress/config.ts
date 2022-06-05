2140334cedc2692ec4643387f7236fd9f33dfa2a
xiupos <me@xiupos.net>
2022-06-05 16:45:29 +0900

    add nav

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,9 @@
+module.exports = {
+  title: '北大言語学サークル Huling',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Twitter', link: 'https://twitter.com/HU_Linguistic' },
+    ],
+  }
+}
