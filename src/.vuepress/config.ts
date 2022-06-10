a092528747ea71f960d966aee71f6b6e903c673b
xiupos <me@xiupos.net>
2022-06-10 18:59:43 +0900

    add about on nav

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,6 +2,7 @@ module.exports = {
   title: '北大言語学サークル Huling',
   themeConfig: {
     nav: [
+      { text: 'About', link: '/about'},
       { text: 'Twitter', link: 'https://twitter.com/HU_Linguistic' },
     ],
   }
