3d9d0c6e5abccf15cd0ba442a1b615fad4a21fc0
xiupos <me@xiupos.net>
2022-06-10 18:55:14 +0900

    Remove Home on nav

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,6 @@ module.exports = {
   title: '北大言語学サークル Huling',
   themeConfig: {
     nav: [
-      { text: 'Home', link: '/' },
       { text: 'Twitter', link: 'https://twitter.com/HU_Linguistic' },
     ],
   }
