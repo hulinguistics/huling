5a42b58791452dd254ad15ec44b4753a6af404f4
xiupos <me@xiupos.net>
2022-08-24 09:52:28 +0900

    add comment component

---
+++ src/.vitepress/theme/components/Comment.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div id="comment-light"></div>
+    <div id="comment-dark"></div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'utterances-comment',
+  methods: {
+    initValine(id, theme) {
+      let utterances = window.document.createElement('script');
+      utterances.type = 'text/javascript';
+      utterances.src = 'https://utteranc.es/client.js';
+      utterances.async = true;
+      utterances.setAttribute('repo', 'hulinguistics/huling');
+      utterances.setAttribute('label', '🕊️comment');
+      utterances.setAttribute('issue-term', 'pathname');
+      utterances.setAttribute('theme', theme);
+      utterances.crossOrigin = 'anonymous';
+      window.document.getElementById(id).appendChild(utterances);
+    },
+  },
+  mounted() {
+    this.initValine('comment-light', 'github-light');
+    this.initValine('comment-dark', 'github-dark');
+  },
+};
+</script>
+
+<style scoped>
+#comment-light,
+#comment-dark {
+  margin-top: 30px;
+}
+html.dark #comment-light,
+html:not(.dark) #comment-dark {
+  display: none;
+}
+</style>
