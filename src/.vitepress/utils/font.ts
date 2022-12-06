341fd2ed0f29e3f94067eb3753958b5ee45ab385
xiupos <me@xiupos.net>
2022-12-06 21:33:40 +0900

    subsetting fonts from font.config.json

--- src/.vitepress/utils/font.ts
+++ src/.vitepress/utils/font.ts
@@ -11,23 +11,12 @@ const arg = process.argv[3];
 // アスキー文字のセット
 const ascii = '!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
 
-// 元フォントのURLのリスト
-const fontUrls = [
-  'https://github.com/notofonts/noto-fonts/raw/main/hinted/ttf/NotoSans/NotoSans-Regular.ttf',
-  'https://github.com/notofonts/noto-fonts/raw/main/hinted/ttf/NotoSans/NotoSans-Italic.ttf',
-  'https://github.com/notofonts/noto-fonts/raw/main/hinted/ttf/NotoSans/NotoSans-Medium.ttf',
-  'https://github.com/notofonts/noto-fonts/raw/main/hinted/ttf/NotoSans/NotoSans-MediumItalic.ttf',
-  'https://github.com/notofonts/noto-fonts/raw/main/hinted/ttf/NotoSans/NotoSans-SemiBold.ttf',
-  'https://github.com/notofonts/noto-fonts/raw/main/hinted/ttf/NotoSans/NotoSans-SemiBoldItalic.ttf',
-  'https://github.com/googlefonts/noto-cjk/raw/main/Sans/OTF/Japanese/NotoSansCJKjp-Regular.otf',
-  'https://github.com/googlefonts/noto-cjk/raw/main/Sans/OTF/Japanese/NotoSansCJKjp-Bold.otf',
-  'https://github.com/googlefonts/noto-cjk/raw/main/Sans/OTF/SimplifiedChinese/NotoSansCJKsc-Regular.otf',
-  'https://github.com/googlefonts/noto-cjk/raw/main/Sans/OTF/SimplifiedChinese/NotoSansCJKsc-Bold.otf',
-  'https://github.com/googlefonts/noto-cjk/raw/main/Sans/OTF/TraditionalChinese/NotoSansCJKtc-Regular.otf',
-  'https://github.com/googlefonts/noto-cjk/raw/main/Sans/OTF/TraditionalChinese/NotoSansCJKtc-Bold.otf',
-];
+// font.config.json
+const configPath = 'src/.vitepress/theme/font.config.json';
 
-const fontDir = 'src/public/font/';
+// サブセットフォントURL
+const fontUrl = '/font/';
+const publicDirPath = 'src/public/';
 
 const typeList = (typ: string) => {
   switch (typ) {
@@ -62,22 +51,29 @@ async function getCharaList(parent: string) {
 }
 
 async function createSubsetFont(parent: string) {
+  const fontDir = path.join(publicDirPath, fontUrl);
   fs.existsSync(fontDir) && fs.removeSync(fontDir);
   fs.mkdirsSync(fontDir);
   const charas = await getCharaList(parent);
-  return await Promise.all(
-    fontUrls.map(async (fontUrl) => {
+
+  const config = JSON.parse(await fs.readFile(configPath, 'utf-8'));
+
+  for (const subset of config.subsets) {
+    for (const font of subset.fonts) {
       // 元フォントを読み込む
-      const buffer = Buffer.from(await (await fetch(fontUrl)).arrayBuffer());
+      const buffer = Buffer.from(await (await fetch(font.url)).arrayBuffer());
 
       // サブセットを生成
       const subsetBuffer = await subsetFont(buffer, charas, { targetFormat: 'woff2' });
 
       // サブセットの書き込み
-      const fontPath = path.join(fontDir, path.basename(fontUrl, path.extname(fontUrl)) + '.woff2');
+      const fontPath = path.join(fontDir, path.basename(font.url, path.extname(font.url)) + '.woff2');
       fs.writeFileSync(fontPath, subsetBuffer);
 
-      return fontPath;
-    }),
-  );
+      // Pathをconfigに追加
+      font.path = path.join(fontUrl, path.basename(font.url, path.extname(font.url)) + '.woff2');
+    }
+  }
+
+  return config;
 }
