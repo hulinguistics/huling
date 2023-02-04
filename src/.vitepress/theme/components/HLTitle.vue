253cf57cbced19fefb9f038b3e6664e423d4e842
xiupos <me@xiupos.net>
2023-02-04 12:07:25 +0900

    タイトルを太字に

--- src/.vitepress/theme/components/HLTitle.vue
+++ src/.vitepress/theme/components/HLTitle.vue
@@ -32,6 +32,11 @@ export default {
   letter-spacing: -0.02em;
   line-height: 40px;
   font-size: 28px;
+  position: relative;
+  font-weight: 600;
+  outline: none;
+  overflow-wrap: break-word;
+  margin: 0;
 
   @media (min-width: 768px) {
     letter-spacing: -0.02em;
