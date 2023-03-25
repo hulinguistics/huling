924a0707a2f271bf84f1341c93e989b7a38498ed
Miyadaj <quruliguruli@gmail.com>
2023-03-25 16:54:24 +0900

    allow regex

--- src/.vitepress/theme/components/HLConverter.vue
+++ src/.vitepress/theme/components/HLConverter.vue
@@ -118,10 +118,12 @@ export default {
       let output: string = input;
       while (
         set.some((value: [string, string]) => {
-          if (value[0] && output.indexOf(value[0]) !== -1) {
-            const output_old = output;
-            output = output.replaceAll(value[0], value[1]);
-            return output != output_old;
+          const output_old = output;
+          const isReg = /\/.+\//.test(value[0]);
+          const key = isReg ? value[0].replace(/\/(.+)\//, '$1') : value[0];
+          if (key) {
+            output = output.replace(isReg ? new RegExp(key, 'gu') : key, value[1]);
+            return output !== output_old;
           } else {
             return false;
           }
